29b437e047801bd8341b98b3f5510aa04714be51
Dominik Ballentin <dominik.ballentin@stud.hs-hannover.de>
2026-02-09 23:42:52 +0000

    drzu

--- app/Excel_eintrag.py
+++ app/Excel_eintrag.py
@@ -3,15 +3,15 @@ from openpyxl import load_workbook
 
 # Excel-Datei laden
 
-def excel_setter(i,ws, datum, decimal_hours, description,First_day, Last_day):
+def excel_setter(i,ws, datum, decimal_hours, description,First_day, Last_day, stundensatz=19):
     
     # Zeile 31 befüllen
     ws[f"A{i}"] = datum     # Datum
     ws[f"B{i}"] = decimal_hours                # Stunden
     ws[f"C{i}"] = description        # Leistung
-    ws[f"D{i}"] = 19
+    ws[f"D{i}"] = stundensatz
     ws[f"E{i}"] = f"=ROUND(B{i}*$D$32,2)"  
-    ws["C24"] = f"{First_day} bist {Last_day}"
+    ws["C24"] = f"{First_day} bis {Last_day}"
 #asd
 # Datei speichernd
 
