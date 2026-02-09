efbcbdbb4b1ca9d6be7e40c4ff1d4fe592f8faaa
Dominik Ballentin <dominik.ballentin@stud.hs-hannover.de>
2026-02-09 21:20:05 +0000

    d

--- app/Excel_eintrag.py
+++ app/Excel_eintrag.py
@@ -4,12 +4,14 @@ from openpyxl import load_workbook
 wb = load_workbook("app/Muster_Honorarrechnung-Lehrkräfte_pytest.xlsx")
 ws = wb.active
 
+for i in range(32,33):
+    
 # Zeile 31 befüllen
-ws["A31"] = "02/04/2026"          # Datum
-ws["B31"] = 0.75                  # Stunden
-ws["C31"] = "Vorbereitung"        # Leistung
-ws["D31"] = 19                    # Stundensatz
-           
+    ws[f"A{i}"] = "02/04/2026"          # Datum
+    ws[f"B{i}"] = 0.75                  # Stunden
+    ws[f"C{i}"] = "Vorbereitung wie gewünscht"        # Leistung
+    ws[f"D{i}"] = 19                    # Stundensatz
+            
 #asd
-# Datei speichern
+# Datei speichernd
 wb.save("Test_Eintrag.xlsx")
