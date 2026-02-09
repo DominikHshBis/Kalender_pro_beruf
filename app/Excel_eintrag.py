214fd6c32258ab9cf226b129207681640cad5cbe
Dominik Ballentin <dominik.ballentin@stud.hs-hannover.de>
2026-02-09 21:48:56 +0000

    mmnm

--- app/Excel_eintrag.py
+++ app/Excel_eintrag.py
@@ -4,14 +4,14 @@ from openpyxl import load_workbook
 wb = load_workbook("app/Muster_Honorarrechnung-Lehrkräfte_pytest.xlsx",data_only=False)
 ws = wb.active
 
-for i in range(32,34):
+for i in range(32,35):
     
 # Zeile 31 befüllen
     ws[f"A{i}"] = "02/04/2026"          # Datum
     ws[f"B{i}"] = 0.75                  # Stunden
     ws[f"C{i}"] = "Vorbereitung wie gewünscht"        # Leistung
     ws[f"D{i}"] = 19
-    ws[f"E{i}"] = f"=ROUND(B{i}*$D$32,2)"         # Berechnung der Kosten
+    ws[f"E{i}"] = f"=ROUND(B{i}*$D${i},2)"         # Berechnung der Kosten
                    # Stundensatz
             
 #asd
