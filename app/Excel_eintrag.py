d371136c197205e43366c269545f62c79816ec57
Dominik Ballentin <dominik.ballentin@stud.hs-hannover.de>
2026-02-15 00:18:55 +0000

    excel, angepasst

--- app/Excel_eintrag.py
+++ app/Excel_eintrag.py
@@ -6,6 +6,7 @@ from openpyxl import load_workbook
 def excel_setter(i,ws, datum, decimal_hours, description,First_day, Last_day, stundensatz=19):
     
     # Zeile 31 befüllen
+   
     ws[f"A{i}"] = datum     # Datum
     ws[f"B{i}"] = decimal_hours                # Stunden
     ws[f"C{i}"] = description        # Leistung
@@ -13,6 +14,7 @@ def excel_setter(i,ws, datum, decimal_hours, description,First_day, Last_day, st
     ws[f"E{i}"] = f"=ROUND(B{i}*$D$32,2)"  
     ws["C24"] = f"{First_day} bis {Last_day}"
     ws["E23"] = f"{Last_day}"
+    ws["A29"] = f"für die unten aufgeführten Leistungen als Honorardozent:in im Projekt Assistierte Ausbildung AsA VIII berechne ich Ihnen wie folgt: "
 #asd
 # Datei speichernd
 
